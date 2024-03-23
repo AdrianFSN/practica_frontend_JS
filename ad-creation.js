5a2c2a464d2172cd098bfc65894b031eb4fac4b9
Adrián S <adriansotes@gmail.com>
2024-03-23 08:30:43 +0100

    feat: new custom event checkNoToken. Used to avoid actions when user is not logged in

--- ad-creation.js
+++ ad-creation.js
@@ -2,14 +2,26 @@ import { sessionController } from "./session/session-controller.js";
 import { notificationController } from "./notifications/notifications-controller.js"
 import { spinnerController } from "./spinner/spinner-controller.js";
 import { insertAdController } from "./ad-creation/ad-creation-controller.js";
+import { checkNoToken } from "./utils/checkToken.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const notification = document.querySelector('.notification');
+    const { showNotification } = notificationController(notification);
+
     const token = localStorage.getItem('token');
+
     if (!token) {
+        document.addEventListener('no-token', (event) => {
+            showNotification(event.detail.message, event.detail.type);
+            event.stopPropagation();
+        });
+
         setTimeout(() => {
             window.location.href = './index.html';
         }, 2000);
+
+        checkNoToken('no-token', document);
     };
 
     const session = document.querySelector('#session');
@@ -18,8 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const spinner = document.querySelector('#spinner');
     const { showSpinner, hideSpinner } = spinnerController(spinner);
 
-    const notification = document.querySelector('.notification');
-    const { showNotification } = notificationController(notification);
+
 
 
     const insertAdFormNode = document.querySelector('#insertAd')
